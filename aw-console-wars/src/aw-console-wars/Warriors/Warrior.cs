fbeb74428e5ac0c6d8d8f3de4444cd1d31d8cd07
Mattias Liljenzin <mattias.liljenzin@academicwork.se>
2017-06-15 20:52:30 +0200

    fix

--- aw-console-wars/src/aw-console-wars/Warriors/Warrior.cs
+++ aw-console-wars/src/aw-console-wars/Warriors/Warrior.cs
@@ -9,21 +9,20 @@ namespace aw_console_wars.Warriors
             AttackHandler.Execute(this, warrior);
         }
 
-        public static string GetDescription(Warrior warrior)
+        public string GetDescription()
         {
             return $@"
-                    Name: \t{warrior.Name}{Environment.NewLine}
-                    Health: \t{warrior.GetHealthAsPercentage()}{Environment.NewLine}
-                    Max health: \t{warrior.MaxHealth}{Environment.NewLine}
-                    Strength: \t{warrior.Attributes.Strength}{Environment.NewLine}
-                    Dexterity: \t{warrior.Attributes.Dexterity}{Environment.NewLine}
-                    Constitution: {warrior.Attributes.Constitution}{Environment.NewLine}";
+                    Name: \t{Name}{Environment.NewLine}
+                    Health: \t{GetHealthAsPercentage()}{Environment.NewLine}
+                    Max health: \t{Attributes.MaxHealth}{Environment.NewLine}
+                    Strength: \t{Attributes.Strength}{Environment.NewLine}
+                    Dexterity: \t{Attributes.Dexterity}{Environment.NewLine}
+                    Constitution: {Attributes.Constitution}{Environment.NewLine}";
         }
 
-        public string GetHealthAsPercentage() => $"{CurrentHealth / MaxHealth:P}%";
+        public string GetHealthAsPercentage() => $"{CurrentHealth / Attributes.MaxHealth:P}%";
 
         public abstract string Name { get; }
-        public abstract int MaxHealth { get; }
         public abstract int CurrentHealth { get; set; }
 
         public bool IsAlive => CurrentHealth > 0;
@@ -32,7 +31,7 @@ namespace aw_console_wars.Warriors
 
         public DamageReport GetStatistics()
         {
-            return new DamageReport(0, MaxHealth - CurrentHealth);
+            return new DamageReport(0, Attributes.MaxHealth - CurrentHealth);
         }
     }
 }
