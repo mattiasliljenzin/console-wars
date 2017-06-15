aacc2aa078b299bed352c47d3995f3a439a1979b
Mattias Liljenzin <mattias.liljenzin@academicwork.se>
2017-06-15 19:49:27 +0200

    abstract warriors only

--- aw-console-wars/src/aw-console-wars/WarriorGenerator.cs
+++ aw-console-wars/src/aw-console-wars/WarriorGenerator.cs
@@ -7,6 +7,7 @@ namespace aw_console_wars
         public Warrior[] GenerateWarriors(int numberOfWarriors)
         {
             GameOutput.Report($"Generating {numberOfWarriors} warriors!");
+
             throw new System.NotImplementedException();
         }
     }
